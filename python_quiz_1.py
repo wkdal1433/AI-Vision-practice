a7cfec1f04b05f6ffc595dda51c040fda0f0dd9d
wkdal1433 <wkdal1433@gmail.com>
2023-02-28 01:29:14 +0900

    freshman first commit

--- python_quiz_1.py
+++ python_quiz_1.py
@@ -5,7 +5,15 @@ youtuberlist = ["justlikethatkr", "paka", "ralo", "dopa"]
 for index in youtuberlist:
     # youtubername = youtuberlist(index)
     f = open("C:/Users/wkdal/Desktop/python quiz/"+str(index)+".txt", 'w')
-    f.write('안녕하세요?? '+str(index)+'님.\n(주)나도출판 편집자 나코입니다.\n현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다. \n'+str(index)+'님의 유튜브 영상을 보고 연락을 드리게 되었습니다. \n자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다. \n\n좋은 하루 보내세요 ^^ \n감사합니다.')
+    f.writelines('안녕하세요?? '+str(index)+'님.')
+    f.writelines('(주)나도출판 편집자 나코입니다.')
+    f.writelines('현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다.')
+    f.writelines(str(index)+'님의 유튜브 영상을 보고 연락을 드리게 되었습니다.')
+    f.writelines('자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다.')
+    f.writelines()
+    f.writelines('좋은 하루 보내세요 ^^')
+    f.writelines('감사합니다.')
+
     f.close()
     
 
