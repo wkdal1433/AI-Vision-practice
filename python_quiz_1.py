292f97946ff0dd71c8e18e0fb94509003aca9b1f
wkdal1433 <wkdal1433@gmail.com>
2023-02-28 02:03:10 +0900

    freshman third quiz commit

--- python_quiz_1.py
+++ python_quiz_1.py
@@ -4,16 +4,14 @@ youtuberlist = ["justlikethatkr", "paka", "ralo", "dopa"]
 
 for index in youtuberlist:
     with open("{}.txt".format(index),'w',encoding="utf8") as email_file:
-        email_file.write(f"""
-        안녕하세요? {index}님.
-        (주)나도출판 편집자 나코입니다.
-        현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다.
-        {index}님의 유튜브 영상을 보고 연락을 드리게 되었습니다.
-        자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다.
-        
-        좋은 하루 보내세요 ^^
-        감사합니다.
-        """)
+        contents = (f"안녕하세요? {index}님.\n\n"
+        "(주)나도출판 편집자 나코입니다.\n"
+        "현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다.\n"
+        f"{index}님의 유튜브 영상을 보고 연락을 드리게 되었습니다.\n"
+        "자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다.\n\n"
+        "좋은 하루 보내세요 ^^\n"
+        "감사합니다.\n")
+        email_file.write(contents)
 
 
 
